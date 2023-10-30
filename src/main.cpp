42544bd86b29df47e107df9ae05e6f5ede473e33
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2023-10-30 17:42:30 -0400

    NCurses

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <ncurses.h>
+#include <unistd.h>
 #include "menu.h"
 #include "colors.h"
 #include "helpers.h"
@@ -7,7 +9,24 @@ using namespace std;
 Menu MainMenu;
 
 int main(int argc, char* argv[]) {
+	initscr();
+	cbreak();
+	noecho();
+	nodelay(stdscr, true);
+	refresh();
 	clearScreen();
+	int ch;
+	while (true) {
+		ch = getch();
+		if (ch == 15) {
+			clearScreen();
+			MainMenu.display();
+			MainMenu.handleInput();
+		}
+		refresh();
+		usleep(100000);
+	}
+	endwin();
 	string welcomeText = colorRed + "Hello Coder & Welcome to LTC." + endColor;
 	typeText(welcomeText, 50);
 	MainMenu.display();
